dd640cf476c29f6515abe63a7db4d98afcc30b2a
Sachin <007mishrasachinmishra@gmail.com>
2023-06-07 16:00:21 +0530

    added some updates

--- app.py
+++ app.py
@@ -9,12 +9,12 @@ WHITE = (255, 255, 255)
 BLACK = (0,0,0)
 pygame.init()
 
-IMAGESAVE =False
+IMAGESAVE = False
 Boundaryinc = 5
 Windowsizex = 640
 Windowsizey = 480
 PREDICT = True
-MODEL = load_model("E:\Programs 2023\ML Projects\image processing\Model\my_model.h5")
+MODEL = load_model("E:\OIP23\Work1\image processing\Model\my_model.h5")
 Displaysurface = pygame.display.set_mode((640,480))
 white_int = Displaysurface.map_rgb(WHITE)
 pygame.display.set_caption("My project")
@@ -45,25 +45,26 @@ while True:
             number_xcord = sorted(number_xcord)
             number_ycord = sorted(number_ycord)
 
-            rect_min_x, rect_max_x = max(number_xcord[0]-Boundaryinc,0),min(Windowsizex, number_xcord+Boundaryinc)
-            rect_min_Y, rect_max_Y = max(number_ycord[0]-Boundaryinc,0),min(Windowsizey, number_ycord+Boundaryinc)
+            rect_min_x, rect_max_x = max(number_xcord[0]- Boundaryinc,0),min(Windowsizex, number_xcord[-1]+Boundaryinc)
+            rect_min_Y, rect_max_Y = max(number_ycord[0]-Boundaryinc,0),min(Windowsizey, number_ycord[-1]+Boundaryinc)
 
             number_xcord = []
             number_ycord = []
 
             img_arr = np.array(pygame.PixelArray(Displaysurface))
             if IMAGESAVE:
-                cv2.imwrite("imagE.png")
+                cv2.imwrite("image.png")
                 inmg_cnt +=1
             
             if PREDICT:
                 image = cv2.resize(img_arr,(28,28))
                 image = np.pad(image, (10,10), 'constant', constant_values=0)
                 image = cv2.resize(image, (28,28))/white_int
-                label = str(label[np.argmax(MODEL.predict(image.reshape(28,28,1)))]).title()
+                label = str(LABELS[np.argmax(MODEL.predict(image.reshape(28,28,1)))]).title()
                 pygame.draw.rect(Displaysurface, RED, (rect_min_x, rect_min_Y, rect_max_x,rect_min_Y, rect_max_Y-rect_min_Y), 3)
             
             if event.type == KEYDOWN:
                 if event.unicode =='N':
                     Displaysurface.fill(BLACK)
+                    
         pygame.display.update()
